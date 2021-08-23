323a665973386ec075c4fae563602051de2fcb81
Dzianis <c.o.den@tut.by>
2021-08-23 12:22:57 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const strInt = Math.abs(n).toString();
+	let result = '';
+	for (let i = 0; i < strInt.length; i++) {
+		result = `${strInt[i]}${result}`
+	}
+	return Number(result);
 }
